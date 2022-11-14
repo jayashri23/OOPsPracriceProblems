5a465c43e49fbe9ee1bad4b9f80effa93e30dbbd
jayashri23 <jsjankar23@gmail.com>
2022-11-14 09:47:11 +0530

    Welcome To Object Oriented Programs

---
+++ src/com/oopspraciceproblems/welcome.java
@@ -0,0 +1,7 @@
+package com.oopspraciceproblems;
+
+public class welcome {
+    public static void main(String [] args){
+        System.out.println("Welcome To Object Oriented Practice Problems");
+    }
+}
